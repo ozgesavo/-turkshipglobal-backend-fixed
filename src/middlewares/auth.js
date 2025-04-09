59760bd882baed4654d9fafa551b50383340e752
ozgesavo <161982657+ozgesavo@users.noreply.github.com>
2025-04-09 18:44:55 -0400

    Update auth.js

--- src/middlewares/auth.js
+++ src/middlewares/auth.js
@@ -4,6 +4,7 @@ module.exports = function(req, res, next) {
   // Get token from header
   const token = req.headers['x-auth-token'] || req.get('x-auth-token');
 
+
   // Check if no token
   if (!token) {
     return res.status(401).json({ msg: 'No token, authorization denied' });
