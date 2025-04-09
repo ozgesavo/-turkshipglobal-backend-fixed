97ce0a72906c9b58a6219bbd670643df904c56d0
ozgesavo <161982657+ozgesavo@users.noreply.github.com>
2025-04-09 18:08:49 -0400

    Update auth.js

--- src/middlewares/auth.js
+++ src/middlewares/auth.js
@@ -2,7 +2,7 @@ const jwt = require('jsonwebtoken');
 
 module.exports = function(req, res, next) {
   // Get token from header
-  const token = req.header('x-auth-token');
+  const token = req.headers['x-auth-token'] || req.get('x-auth-token');
 
   // Check if no token
   if (!token) {
